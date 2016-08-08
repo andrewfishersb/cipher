36800d90967ef7322ca0a58654a72ac652809af9
Andrew Fisher and Garrett Boggs <andrew@>
2016-08-08 13:52:01 -0700

    Adds all the javascript functions

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+
+var firstLast = function(aSentence){
+  var twoStrings= aSentence.charAt(0).toUpperCase() + aSentence.charAt(aSentence.length-1).toUpperCase();
+  return twoStrings;
+}
+
+var reverse = function(tletter)
+{
+  return (tletter.slice(1,2) + tletter.slice(0,1));
+}
+
+
+var math = function(letters){
+  return letters.charAt((letters.length)/2)+letters;
+}
+
+var cipher = function(anotherSentence){
+  var cheat = anotherSentence;
+  var twoSmash = firstLast(cheat);
+  var reversesmash = reverse(twoSmash);
+  return anotherSentence.concat(reversesmash);
+}
+
+var reverse2 = function(str){
+
+  var ans = "";
+  for(var i = (str.length-1); i >= 0; i --)
+  {
+    ans = ans + str.charAt(i);
+  }
+
+  return ans;
+}
+
+
+var giveMeASentence = prompt("Enter any sentence");
+
+var string = cipher(giveMeASentence);
+alert(string);
+
+alert(math(string));
+alert(reverse2(math(string)));
